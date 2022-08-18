aaffd5a9d6a1abcc77c45d78e9ab5514d0ccf7b2
ThiAABrown <antunesabrown@gmail.com>
2022-08-18 16:34:18 -0300

    Criado testes para funcao converter_humano

--- tests/core/test_funcoes.py
+++ tests/core/test_funcoes.py
@@ -1,8 +1,16 @@
-from core.funcoes import calcular_estatisticas, converter
+from core.funcoes import calcular_estatisticas, converter, converter_humano
 
 def test_calcular_estatisticas():
     # TODO: Criar um teste para a funcao calcular_estatisticas usando o ex: http://127.0.0.1:8000/estatisticas/12/6/
     assert 1==1
+
+def test_converter_humano_ex1():
+    converterhumanoex1 = converter_humano('00:00:15')
+    assert converterhumanoex1 == '00 horas, 00 minutos e 15 segundos '
+
+def test_converter_humano_ex2():
+    converterhumanoex2 = converter_humano('00:11:40')
+    assert converterhumanoex2 == '00 horas, 11 minutos e 40 segundos '
 
 def test_converter_ex1():
     converterex1 = converter(15)
